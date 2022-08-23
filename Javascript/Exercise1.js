fbc317e6cf201ba224307ac8640ae40ceed22234
errikos22 <esimintiridis@gmail.com>
2022-08-23 21:48:17 +0300

    More

--- Javascript/Exercise1.js
+++ Javascript/Exercise1.js
@@ -1,17 +1,19 @@
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let d = new Date().getDay();
+let date = new Date("1995-12-17T00:00:00");
+let d = date.getDay();
 let day = days[d];
 
-let hour = new Date().getHours();
-let minute = new Date().getMinutes();
-let second = new Date().getSeconds();
-let hour2= hour - 12 ;
-if (hour < 12)  {
-  hour = new Date().getHours() + " AM";
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+if (hour < 12) {
+  hour = hour + " AM";
 } 
-else (hour > 12) ; {
-  hour = hour2 + " PM";
+else if (hour===0 && minute===0 && second===0) {
+hour="Midnight";
+}
+else {
+  hour = hour-12  + " PM";
 }
 
-let x="Today is : " + day + ". Current Time is : " + hour + " : " + minute + " : " + second + ".";
-console.log(x);
+document.write("Today is : " + day + ".<br> Current Time is : " + hour + " : " + minute + " : " + second + ".");
